e351d646ffd5400eadd2b6c6439c53b4db91d1e7
Leon Atherton <leon@idrsolutions.com>
2020-08-06 14:19:30 +0100

    Inline the Libreoffice errors & tidy up settings code

--- src/main/java/com/idrsolutions/microservice/JPedalServlet.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServlet.java
@@ -86,8 +86,8 @@ public class JPedalServlet extends BaseServlet {
     protected void convert(Individual individual, Map<String, String[]> params,
                            File inputFile, File outputDir, String contextUrl) {
 
-        final Map<String, String> conversionParams = individual.getConversionParams() != null
-                ? individual.getConversionParams() : new HashMap<>();
+        final Map<String, String> conversionParams = individual.getSettings() != null
+                ? individual.getSettings() : new HashMap<>();
 
         final String fileName = inputFile.getName();
         final String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
@@ -100,9 +100,7 @@ public class JPedalServlet extends BaseServlet {
 
         final boolean isPDF = ext.toLowerCase().endsWith("pdf");
         if (!isPDF) {
-            final int result = convertToPDF(inputFile);
-            if (result != 0) {
-                setErrorCode(individual, result);
+            if (!convertToPDF(inputFile, individual)) {
                 return;
             }
             userPdfFilePath = inputDir + fileSeparator + fileNameWithoutExt + ".pdf";
@@ -155,19 +153,16 @@ public class JPedalServlet extends BaseServlet {
     @Override
     protected boolean validateRequest(final HttpServletRequest request, final HttpServletResponse response,
                                       final Individual individual) {
-        final String settings = request.getParameter("settings");
-        Map<String, String> convParams = null;
 
-        if (settings != null) {
-            try {
-                convParams = parseConversionParams(settings);
-            } catch (JsonParsingException exception) {
-                doError(request, response, "Error encountered when parsing settings JSON <" + exception.getMessage() + ">", 400);
-                return false;
-            }
+        final Map<String, String> settings;
+        try {
+            settings = parseSettings(request.getParameter("settings"));
+        } catch (JsonParsingException exception) {
+            doError(request, response, "Error encountered when parsing settings JSON <" + exception.getMessage() + '>', 400);
+            return false;
         }
 
-        final SettingsValidator settingsValidator = new SettingsValidator(convParams);
+        final SettingsValidator settingsValidator = new SettingsValidator(settings);
 
         final String mode = settingsValidator.validateString("mode", validModes, true);
         if (mode != null) {
@@ -188,7 +183,7 @@ public class JPedalServlet extends BaseServlet {
             return false;
         }
 
-        individual.setConversionParams(convParams);
+        individual.setSettings(settings);
 
         return true;
     }
@@ -241,35 +236,13 @@ public class JPedalServlet extends BaseServlet {
     }
 
     /**
-     * Set the error code in the given individual object. Error codes are based
-     * on the return values of 
-     * {@link JPedalServlet#convertToPDF(File)}
-     *
-     * @param individual the individual object associated with this conversion
-     * @param errorCode The return code to be parsed to an error code
-     */
-    private void setErrorCode(final Individual individual, final int errorCode) {
-        switch (errorCode) {
-            case 1:
-                individual.doError(1050, "Libreoffice timed out after 1 minute"); // Libreoffice killed after 1 minute
-                break;
-            case 2:
-                individual.doError(1070, "Internal error processing file"); // Internal error
-                break;
-            default:
-                individual.doError(1100, "An internal error has occurred"); // Internal error
-                break;
-        }
-    }
-
-    /**
      * Converts an office file to PDF using LibreOffice.
      *
      * @param file The office file to convert to PDF
-     * @return 0 if success, 1 if libreoffice timed out, 2 if process error
-     * occurs
+     * @param individual The Individual on which to set the error if one occurs
+     * @return true on success, false on failure
      */
-    private static int convertToPDF(final File file) {
+    private static boolean convertToPDF(final File file, final Individual individual) {
         final ProcessBuilder pb = new ProcessBuilder("soffice", "--headless", "--convert-to", "pdf", file.getName());
         pb.directory(new File(file.getParent()));
         final Process process;
@@ -278,13 +251,15 @@ public class JPedalServlet extends BaseServlet {
             process = pb.start();
             if (!process.waitFor(1, TimeUnit.MINUTES)) {
                 process.destroy();
-                return 1;
+                individual.doError(1050, "Libreoffice timed out after 1 minute");
+                return false;
             }
         } catch (final IOException | InterruptedException e) {
             e.printStackTrace(); // soffice location may need to be added to the path
             LOG.severe(e.getMessage());
-            return 2;
+            individual.doError(1070, "Internal error processing file");
+            return false;
         }
-        return 0;
+        return true;
     }
 }
