7734c779f86f28b636aabf29e0d4aefc2af2bd5c
Daniel Warren <daniel@idrsolutions.com>
2021-01-14 11:58:47 +0000

    Update Copyright year across project

--- src/main/java/com/idrsolutions/microservice/JPedalServlet.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServlet.java
@@ -3,7 +3,7 @@
  *
  * Project Info: https://github.com/idrsolutions/jpedal-microservice-example
  *
- * Copyright 2020 IDRsolutions
+ * Copyright 2021 IDRsolutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
