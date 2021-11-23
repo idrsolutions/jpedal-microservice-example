dfa67f8caf69997fd0b805764b500bf87bffe7f0
Kieran-IDR <kieran@idrsolutions.com>
2021-11-23 11:32:09 +0000

    Update properties handling to use InputStreams for internal and external files

--- src/main/java/com/idrsolutions/microservice/JPedalServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServletContextListener.java
@@ -2,6 +2,9 @@ package com.idrsolutions.microservice;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @WebListener
 public class JPedalServletContextListener extends BaseServletContextListener {
@@ -12,7 +15,19 @@ public class JPedalServletContextListener extends BaseServletContextListener {
         if (!userDir.endsWith("/") && !userDir.endsWith("\\")) {
             userDir += System.getProperty("file.separator");
         }
-        propertiesFile = userDir + "/.idr/jpedal-microservice/jpedal-microservice.properties";
+
+        final File externalFile = new File(userDir + "/.idr/jpedal-microservice/jpedal-microservice.properties");
+
+        try {
+            if (externalFile.exists()) {
+                propertiesFile = new FileInputStream(externalFile.getAbsolutePath());
+            } else {
+                propertiesFile = servletContextEvent.getServletContext().getResourceAsStream("/WEB_INF/classes/jpedal-microservice.properties");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
         super.contextInitialized(servletContextEvent);
     }
