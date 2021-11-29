63559c45688a611995b606f3f60bf4c73cd32a0f
Kieran-IDR <kieran@idrsolutions.com>
2021-11-29 10:14:13 +0000

    Update the context listeners to move all stream handling into the base class

--- src/main/java/com/idrsolutions/microservice/JPedalServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServletContextListener.java
@@ -1,35 +1,19 @@
 package com.idrsolutions.microservice;
 
-import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 @WebListener
 public class JPedalServletContextListener extends BaseServletContextListener {
 
-    @Override
-    public void contextInitialized(final ServletContextEvent servletContextEvent) {
+    public String getConfigPath() {
         String userDir = System.getProperty("user.home");
         if (!userDir.endsWith("/") && !userDir.endsWith("\\")) {
             userDir += System.getProperty("file.separator");
         }
-
-        final File externalFile = new File(userDir + "/.idr/jpedal-microservice/jpedal-microservice.properties");
-
-        try {
-            if (externalFile.exists()) {
-                propertiesFile = new FileInputStream(externalFile.getAbsolutePath());
-            } else {
-                propertiesFile = servletContextEvent.getServletContext().getResourceAsStream("/WEB_INF/classes/jpedal-microservice.properties");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-        super.contextInitialized(servletContextEvent);
+        return userDir + "/.idr/jpedal-microservice/";
     }
 
+    public String getConfigName(){
+        return "jpedal-microservice.properties";
+    }
 }
