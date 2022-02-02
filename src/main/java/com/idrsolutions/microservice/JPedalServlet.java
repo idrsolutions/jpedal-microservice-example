1828e9dd9e6fa3099291b0e3ce28e7b7c2f78d49
Jacob Lucas <Jacob@idrsolutions.com>
2022-02-02 17:17:01 +0000

    Added finality to variables missing it

--- src/main/java/com/idrsolutions/microservice/JPedalServlet.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServlet.java
@@ -93,7 +93,7 @@ public class JPedalServlet extends BaseServlet {
 
         final Map<String, String> conversionParams;
         try {
-            Map<String, String> settings = DBHandler.INSTANCE.getSettings(uuid);
+            final Map<String, String> settings = DBHandler.INSTANCE.getSettings(uuid);
             conversionParams = settings != null ? settings : new HashMap<>();
         } catch (final SQLException e) {
             DBHandler.INSTANCE.setError(uuid, 500, "Database failure");
