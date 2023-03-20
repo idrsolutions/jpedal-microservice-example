ca040ea6d19caaf0d1fe2f61d1e975939ec0c045
Kieran <kieran@idrsolutions.com>
2023-03-20 10:25:51 +0000

    Improve error readability

--- src/main/java/com/idrsolutions/microservice/JPedalServlet.java
+++ src/main/java/com/idrsolutions/microservice/JPedalServlet.java
@@ -208,7 +208,7 @@ public class JPedalServlet extends BaseServlet {
 
         } catch (final Throwable ex) {
             LOG.log(Level.SEVERE, "Exception thrown when converting input", ex);
-            DBHandler.getInstance().setError(uuid, 1220, "Exception thrown when converting input" + ex.getMessage());
+            DBHandler.getInstance().setError(uuid, 1220, "Exception thrown when converting input: " + ex.getMessage());
         }
     }
 
